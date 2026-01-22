e7d7aa8b106d57940045e3857c508fb1cb774a3f
Arian <arian.kulmer@web.de>
2026-01-22 19:18:48 +0100

    avicii: Drop QTI power hal and migrate to libperfmgr
    Change-Id: I1f0a5fcf281f48e9610e521ec0ef07d7e1ae6dc0
    Signed-off-by: lostsignal-502 <viaan4542@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2026-03-01
 
 # SEPolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/oneplus/avicii/sepolicy/qti/SEPolicy.mk
 
--- device.mk
+++ device.mk
@@ -350,9 +350,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service.lineage-libperfmgr
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
@@ -405,7 +403,10 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -476,7 +477,6 @@ SPAMMY_LOG_TAGS := \
     android.hardware.light-V2-ndk.vendor \
     vibratorfeature-wrapper \
     android.hardware.vibrator-V1-ndk_platform.vendor \
-    libqti-perfd-client \
     BATTERY_CHG \
     BackgroundInstallControlService \
     BackupTransportManager \
