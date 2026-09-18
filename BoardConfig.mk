346dcee4957be3b57743e79bceafaf589a2f4dad
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-09-18 07:04:50 +0000

    avicii: Import c2_manifest_vendor.xml
    For Dolby vision

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/c2_manifest_vendor.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
